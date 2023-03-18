afde8ca71b56e47451b1230ca5c28c2689a3f437
Wasim Ahmad <wasimahamed961@gmail.com>
2023-03-18 09:33:20 +0530

    add method,dateTime

--- java_Basics/src/DateTime/SimpleDateTimeFormat.java
+++ java_Basics/src/DateTime/SimpleDateTimeFormat.java
@@ -11,6 +11,14 @@ public class SimpleDateTimeFormat {
 		SimpleDateFormat ft = new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a zzz");
 		
 		System.out.println("Current Date: "+ft.format(date));
+		
+//		another way
+//		Date date = new Date();
+//
+//	      // display time and date
+//	      String str = String.format("Current Date/Time : %tc", date );
+//
+//	      System.out.printf(str);
 	}
 
 }
